72ac02b714ec56956aad1ae58eb746b7e49c1c42
Quinto Cossio <quintocossio@MacBook-Pro-de-Quinto.local>
2016-05-20 19:32:57 -0300

    first commit

---
+++ weather-app/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  weather-app
+//
+//  Created by Quinto Cossio on 16/5/16.
+//  Copyright © 2016 Quinto Cossio. All rights reserved.
+//
+
+import Foundation
---
+++ weather-app/Weather.swift
@@ -0,0 +1,9 @@
+//
+//  Weather.swift
+//  weather-app
+//
+//  Created by Quinto Cossio on 11/5/16.
+//  Copyright © 2016 Quinto Cossio. All rights reserved.
+//
+
+import Foundation
